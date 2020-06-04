2b0d62607f780b919b9959f4b3918b7d48cefc9c
Fabian Gehrlicher <f.gehrlicher@plan-net.com>
2020-06-04 08:54:47 +0200

    Add db tags to the typo3 user group struct

--- api/typo3_user_group.go
+++ api/typo3_user_group.go
@@ -5,35 +5,35 @@ import (
 )
 
 type Typo3UserGroup struct {
-	Uid int
-	Pid int
+	Uid int `db:"uid"`
+	Pid int `db:"pid"`
 
-	Title       string
-	Description string
+	Title       string `db:"title"`
+	Description string `db:"description"`
 
-	Deleted     int
-	Hidden      int
-	HideInLists int
+	Deleted      int    `db:"deleted"`
+	Hidden       int    `db:"hidden"`
+	HideInLists  int    `db:"hide_in_lists"`
+	LockToDomain string `db:"lockToDomain"`
 
-	Tstamp int
-	Crdate int
+	Tstamp int `db:"tstamp"`
+	Crdate int `db:"crdate"`
 
-	WorkspacePerms int
-	CruserID       int
+	WorkspacePerms int `db:"workspace_perms"`
+	CruserID       int `db:"cruser_id"`
 
-	LockToDomain      string
-	TSconfig          sql.NullString
-	AllowedLanguages  string
-	CategoryPerms     sql.NullString
-	CustomOptions     sql.NullString
-	DbMountpoints     sql.NullString
-	ExplicitAllowdeny sql.NullString
-	FileMountpoints   sql.NullString
-	FilePermissions   sql.NullString
-	GroupMods         sql.NullString
-	NonExcludeFields  sql.NullString
-	PagetypesSelect   string
-	Subgroup          sql.NullString
-	TablesModify      sql.NullString
-	TablesSelect      sql.NullString
+	TSconfig          sql.NullString `db:"TSconfig"`
+	AllowedLanguages  string         `db:"allowed_languages"`
+	CategoryPerms     sql.NullString `db:"category_perms"`
+	CustomOptions     sql.NullString `db:"custom_options"`
+	DbMountpoints     sql.NullString `db:"db_mountpoints"`
+	ExplicitAllowdeny sql.NullString `db:"explicit_allowdeny"`
+	FileMountpoints   sql.NullString `db:"file_mountpoints"`
+	FilePermissions   sql.NullString `db:"file_permissions"`
+	GroupMods         sql.NullString `db:"groupMods"`
+	NonExcludeFields  sql.NullString `db:"non_exclude_fields"`
+	PagetypesSelect   string         `db:"pagetypes_select"`
+	Subgroup          sql.NullString `db:"subgroup"`
+	TablesModify      sql.NullString `db:"tables_modify"`
+	TablesSelect      sql.NullString `db:"tables_select"`
 }
